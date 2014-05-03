a95ccc0c420bb270929f9274181e3de350f01cb8
lakmeer <lakmeerkravid@gmail.com>
2014-05-03 19:05:52 +1200

    Fix semicolons on helpers tests

--- test/helpersTests.js
+++ test/helpersTests.js
@@ -9,29 +9,29 @@ describe('helpers', function () {
   describe('#last()', function () {
     it('should return element in int list', function () {
       assert.equal(3, helpers.last([1,2,3]));
-    })
+    });
 
     it('should return null in null list', function () {
       assert.equal(null, helpers.last(null));
-    })
+    });
 
     it('should return null if not passed a list', function () {
       assert.equal(null, helpers.last(123));
-    })
+    });
   });
 
   describe('#getExtension()', function () {
     it('should return extension of file', function () {
       assert.equal('html', helpers.getExtension('index.html'));
-    })
+    });
 
     it('should return null if no extension in input string', function () {
       assert.equal(null, helpers.getExtension('noextension'));
-    })
+    });
 
     it('should always return last set of characters after last \'.\' character', function () {
       assert.equal('html', helpers.getExtension('test.index.html'));
-    })
+    });
   });
 
   describe('#escapeQuotes()', function () {
