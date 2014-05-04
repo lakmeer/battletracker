5bc752251102f4a7a02c3e0a0ac03f808a5ac146
lakmeer <lakmeerkravid@gmail.com>
2014-05-04 14:05:48 +1200

    Add speculative party view layout for testing

--- src/client/party.js
+++ src/client/party.js
@@ -1,12 +1,10 @@
 
 var React = require('react');
 
-var TestComponent = require('./components/testComponent.jsx');
-
 window.onload = function () {
-  React.renderComponent(
-    TestComponent({ text : "TestComponent is working" }),
-    document.getElementById('example')
-  );
+
+  // React.renderComponent(
+  //   document.getElementById('party-view')
+  // );
 };
 
