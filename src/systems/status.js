3b69a9bf0dfafb2a16d672e964cd4582f18f3dfe
lakmeer <lakmeerkravid@gmail.com>
2014-05-27 21:40:14 +1200

    Added status callbacks and expiry

--- src/systems/status.js
+++ src/systems/status.js
@@ -1,29 +1,45 @@
 
+// Require
+
 var ComponentSystem = require('../componentSystem.js');
+
+
+// Helpers
+
+function invoke (entity, status, fnName, args) {
+  var args = [ entity ].concat(args || []);
+
+  if (status.callbacks[fnName]) {
+    status.callbacks[fnName].apply(null, args);
+  }
+}
 
 
 // Status
 
-var statusSystem = new ComponentSystem('status', function () {
+var statusSystem = new ComponentSystem('statuses', function () {
   return [ ];
 });
 
-statusSystem.addMethod('newStatus', function (entity, name, hook, rem) {
-  entity.components.statuses.push {
+statusSystem.addMethod('addStatus', function (entity, name, config) {
+  entity.components.statuses.push({
     name: name,
-    hook : hook,
-    remaining: rem
-  }
+    hook : config.hook,
+    remaining: config.remaining,
+    callbacks : config.callbacks || {}
+  });
 });
 
 statusSystem.addMethod('update', function (entity, hook) {
   entity.components.statuses.forEach(function (status) {
     if (status.hook === hook) {
       status.remaining -= 1;
+      invoke(entity, status, hook);
     }
 
     if (status.remaining <= 0) {
-      // Deal with this
+      status.remaining = 0;
+      invoke(entity, status, 'elapsed');
     }
   });
 });
